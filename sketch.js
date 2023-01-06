aa901d293f898d0f57eb88bada9c403c61915cd6
Alexey Larionov <a.larionov@partner.samsung.com>
2023-01-07 01:25:57 +0300

    Switch to Guify library

--- sketch.js
+++ sketch.js
@@ -15,7 +15,11 @@ let crosshairSettings = new function() {
   this.crosshairEnabled = true;
   this.crosshairLinesEnd = 10;
   this.crosshairRadius = 10;
-  this.crosshairColor = [255, 0, 0];
+  this.crosshairColor = '#ff0000';
+}();
+
+let imageInfo = new function() {
+  this.imageSizeStr = "";
 }();
 
 function getLocalized(key) {
@@ -43,23 +47,47 @@ class ImageAABBView {
   //   this.seenBottom += y;
   // }
 
-  // getScreenPosition(screenWidth, screenHeight) {
-  //   return {
-  //     left: max(0, screenWidth/2-this.seenLeft),
-  //     right: min(screenWidth, screenWidth/2+this.seenRight),
-  //     top: max(0, screenHeight/2-this.seenTop),
-  //     bottom: min(screenHeight, screenHeight/2+this.seenBottom),
-  //   }
-  // }
+  toScreenBbox(screenWidth, screenHeight) {
+    return {
+      left: screenWidth/2-this.imageWidth/2-this.seenX,
+      width: this.imageWidth,
+      top: screenHeight/2-this.imageHeight/2-this.seenY,
+      height: this.imageHeight,
+    }
+    // return {
+    //   left: max(0, screenWidth/2-this.seenLeft),
+    //   right: min(screenWidth, screenWidth/2+this.seenRight),
+    //   top: max(0, screenHeight/2-this.seenTop),
+    //   bottom: min(screenHeight, screenHeight/2+this.seenBottom),
+    // }
+  }
 
   zoomTo(screenX, screenY, zoomDelta, screenWidth, screenHeight) {
-    let screenAdjustX = screenX / screenWidth;
-    let screenAdjustY = screenY / screenHeight;
-    print(screenAdjustX, screenAdjustY)
-    this.seenWidth -= this.imageWidth*zoomDelta;
-    this.seenX += this.seenWidth*screenAdjustX*zoomDelta;
-    this.seenHeight -= this.imageHeight*zoomDelta;
-    this.seenY += this.seenHeight*screenAdjustY*zoomDelta;
+    {
+      let screenAdjustX = screenX / screenWidth;
+      let currWidth = float(this.seenWidth);
+      this.seenWidth -= currWidth*zoomDelta*(2*screenAdjustX - 1);
+      this.seenX += currWidth*screenAdjustX*zoomDelta;
+    }
+    {
+      let screenAdjustY = screenY / screenHeight;
+      let currHeight = float(this.seenHeight);
+      this.seenHeight -= currHeight*zoomDelta*(2*screenAdjustY - 1);
+      this.seenY += currHeight*screenAdjustY*zoomDelta;
+    }
+
+    // if (this.seenX < 0) {
+    //   this.seenWidth += Math.abs(this.seenX);
+    // }
+    // if (this.seenX > screenWidth) {
+    //   this.seenWidth -= Math.abs(screenWidth-this.seenX);
+    // }
+    // if (this.seenY < 0) {
+    //   this.seenHeight += Math.abs(this.seenY);
+    // }
+    // if (this.seenY > screenHeight) {
+    //   this.seenHeight -= Math.abs(screenHeight-this.seenY);
+    // }
   }
 
   toString() {
@@ -67,43 +95,100 @@ class ImageAABBView {
   }
 }
 
+var file = null;
 function setupGui(gui, panel) {
-  let savedGui = undefined;
   if (gui !== undefined) {
-    savedGui = gui.getSaveObject();
-    gui.destroy();
+    let guifyContainers = document.getElementsByClassName('guify-container');
+    for (let element of guifyContainers) {
+        element.remove();
+    }
+    print('Deleted GUI instances');
   }
-  dat.GUI.TEXT_CLOSED = getLocalized('closeControls');
-  dat.GUI.TEXT_OPEN = getLocalized('openControls');
-  gui = new dat.GUI(load=savedGui, autoPlace=false);
-  gui.width = 300;
-  gui.add(panel, 'language', ['RUS', 'ENG'])
-    .name(getLocalized('language'))
-    .onFinishChange(
-      function(){
-        panel.language = this.getValue();
-        setupGui(gui, panel);
-      }
-    );
 
-  let addCrosshairFolder = function(gui) {
-    let crosshairFolder = gui.addFolder(getLocalized('crosshairFolder'));
-    crosshairFolder.add(crosshairSettings, 'crosshairEnabled')
-      .name(getLocalized('crosshairEnabled'))
-    crosshairFolder.add(crosshairSettings, 'crosshairLinesEnd', 1, 50)
-      .name(getLocalized('crosshairLinesEnd'));
-    crosshairFolder.add(crosshairSettings, 'crosshairRadius', 0, 50)
-      .name(getLocalized('crosshairRadius'));
-    crosshairFolder.addColor(crosshairSettings, 'crosshairColor')
-      .name(getLocalized('crosshairColor'));
-    return crosshairFolder;
-  };
-  addCrosshairFolder(gui);
-
-  gui.add(panel, 'openImageFiles')
-    .name(getLocalized('openImageFiles'));
-
+  gui = new guify({
+    title:  getLocalized('title'),
+    theme: 'light', // dark, light, yorha, or theme object
+    align: 'right', // left, right
+    width: 500,
+    barMode: 'offset', // none, overlay, above, offset
+    panelMode: 'inner',
+    panelOverflowBehavior: 'scroll',
+    pollRateMs: 500,
+    opacity: 1.0,
+    open: true
+  });
   document.title = getLocalized('title');
+  gui.Register([
+    {
+      type: 'select',
+      options: ['RUS', 'ENG'],
+      label: getLocalized('language'),
+      object: panel,
+      property: 'language',
+      onChange: (data) => {
+        setupGui(gui, panel);
+      },
+    },
+
+    {
+      type: 'folder',
+      label: getLocalized('crosshairFolder'),
+      open: false
+    },
+
+    {
+      type: 'button',
+      label: getLocalized('openImageFiles'),
+      object: this,
+      // property: 'file',
+      action: () => {
+        gui.Toast('>>> Opened a file');
+        generalSettings.openImageFiles();
+      }
+    },
+    {
+      type: 'text',
+      label: getLocalized('imageSize'),
+      object: imageInfo, property: 'imageSizeStr',
+      listenMode: 'change',
+    }
+  ]);
+
+  gui.Register([
+    {
+      type: 'checkbox',
+      label: getLocalized('crosshairEnabled'),
+      property: 'crosshairEnabled',
+    },
+    {
+      type: 'range',
+      label: getLocalized('crosshairRadius'),
+      min: 0, max: 50, step: 1,
+      property: 'crosshairRadius'
+    },
+
+    {
+      type: 'range',
+      label: getLocalized('crosshairLinesEnd'),
+      min: 1, max: 50, step: 1,
+      property: 'crosshairLinesEnd'
+    },
+
+    {
+      type: 'color',
+      label: getLocalized('crosshairColor'),
+      format: 'hex',
+      property: 'crosshairColor'
+    },
+  ], { object: crosshairSettings, folder: getLocalized('crosshairFolder') });
+  let guifyBarButtons = document.getElementsByClassName('guify-bar-button');
+  for (let element of guifyBarButtons) {
+    if (element.ariaLabel === null || !element.ariaLabel.includes("screen")) {
+        print(element.ariaLabel);
+        element.innerText = getLocalized('controls');
+      }
+  }
+
   return gui;
 }
 
@@ -122,8 +207,10 @@ function preload() {
     .then((data) => localization = data);
   img = loadImage('assets/example_trees/000001.jpg');
 }
+
 function setup() {
   print(img.width, img.height)
+  imageInfo.imageSizeStr = `${img.width} x ${img.height}`;
   createCanvas(windowWidth, windowHeight);
   windowResized();
 
@@ -131,10 +218,10 @@ function setup() {
   imgView = new ImageAABBView(img.width, img.height);
 }
 
-function drawCrosshair(x, y, beginOffset, endOffset, colorRgbArray) {
+function drawCrosshair(x, y, beginOffset, endOffset, colorHexStr) {
   push();
   noFill();
-  stroke(...colorRgbArray);
+  stroke(colorHexStr);
   strokeWeight(3);
   strokeCap(SQUARE);
   line(x + beginOffset, y, x + endOffset, y);
@@ -145,21 +232,33 @@ function drawCrosshair(x, y, beginOffset, endOffset, colorRgbArray) {
   pop();
 }
 
+function drawZoomRect() {
+  push();
+  stroke(255, 0, 0);
+  noFill();
+  let zoomScreenBbox = imgView.toScreenBbox(windowWidth, windowHeight);
+  // print(zoomScreenBbox);
+  rect(zoomScreenBbox.left, zoomScreenBbox.top, zoomScreenBbox.width, zoomScreenBbox.height);
+  pop();
+}
+
 function draw() {
   clear();
+  let zoomScreenBbox = imgView.toScreenBbox(windowWidth, windowHeight);
   image(img,
-    0, 0,
-    windowWidth, windowHeight,
-    imgView.seenX, imgView.seenY,
-    // img.width, img.height,
-    imgView.seenWidth, imgView.seenHeight,
-    CONTAIN);
+    zoomScreenBbox.left, zoomScreenBbox.top, zoomScreenBbox.width, zoomScreenBbox.height
+    );
+    // imgView.seenX, imgView.seenY,
+    // // img.width, img.height,
+    // imgView.seenWidth, imgView.seenHeight);
+
+  drawZoomRect();
 
   if (crosshairSettings.crosshairEnabled) {
     drawCrosshair(mouseX, mouseY,
       beginOffset=crosshairSettings.crosshairRadius,
       endOffset=crosshairSettings.crosshairLinesEnd,
-      colorRgbArray=crosshairSettings.crosshairColor,
+      colorHexStr=crosshairSettings.crosshairColor,
       );
   }
 
