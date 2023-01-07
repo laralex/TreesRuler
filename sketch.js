8a278c0792b8a417e937b77186c8be5840ba63cb
Alexey Larionov <a.larionov@partner.samsung.com>
2023-01-08 00:44:11 +0300

    WIP dynamic measurements system for GUI

--- sketch.js
+++ sketch.js
@@ -2,13 +2,15 @@
 
 let img;
 let localization;
+let gui;
+let guiMeasureComposer;
 var imgView;
 
 let generalSettings = new function() {
   this.crosshairEnabled = true;
   this.language = 'RUS';
   this.guiOpacity = 0.9;
-  this.guiWidth = 275;
+  this.guiWidth = 300;
   this.guiTheme = 'yorha';
   this.guiAskUnsaved = false;
 }();
@@ -63,7 +65,121 @@ class ImageAABBView {
   }
 }
 
-var file = null;
+class MeasureGroupGuiComposer {
+  constructor(imageWidth, imageHeight) {
+    this.groups = [];
+    this.imageWidth = imageWidth;
+    this.imageHeight = imageHeight;
+  }
+
+  _newMeasure({gui=null, parentFolder=null, newFolderName=null, boundData=null, open=false, removable=false}) {
+    let definitions = [
+      {
+        type: 'folder',
+        label: newFolderName,
+        folder: parentFolder,
+        open: open
+      },
+      {
+        type: 'range',
+        label: getLocalized('beginPointX'),
+        min: 0, max: this.imageWidth, step: 1,
+        property: 'x0',
+        object: boundData, folder: newFolderName
+      },
+      {
+        type: 'range',
+        label: getLocalized('beginPointY'),
+        min: 0, max: this.imageHeight, step: 1,
+        property: 'y0',
+        object: boundData, folder: newFolderName
+      },
+      {
+        type: 'range',
+        label: getLocalized('endPointX'),
+        min: 0, max: this.imageWidth, step: 1,
+        property: 'x1',
+        object: boundData, folder: newFolderName
+      },
+      {
+        type: 'range',
+        label: getLocalized('endPointY'),
+        min: 0, max: this.imageHeight, step: 1,
+        property: 'y1',
+        object: boundData, folder: newFolderName
+      }
+    ];
+    let objects = [];
+    for (let definition of definitions) {
+      objects.push(gui.Register(definition));
+    }
+    print(boundData, newFolderName);
+    if (removable) {
+      objects.push(gui.Register({
+        type: 'button',
+        label: getLocalized('guiRemoveMeasure'),
+        folder: newFolderName,
+        action: () => { objects.forEach(obj => gui.Remove(obj)) },
+      }));
+    }
+    return objects;
+  }
+  newGroup(gui, parentFolder) {
+    let uniqueSpaces = '@'.repeat(this.groups.length);
+    let groupFolder = `${getLocalized('guiGroup')}${this.groups.length}`;
+    let groupFolderObj = gui.Register({
+      type: 'folder',
+      label: groupFolder,
+      folder: parentFolder,
+      open: true
+    });
+    let guiObjects = [groupFolderObj];
+    let groupBoundData = {
+      color: "#00FF00",
+      denotation: 'a',
+      baseMeasure: {x0: 0, y0: 0, x1: 0, y1: 0},
+      measures: [{x0: 0, y0: 0, x1: 0, y1: 0}]
+    };
+    guiObjects.push(gui.Register({
+        type: 'color',
+        label: getLocalized('guiGroupColor'),
+        format: 'hex',
+        property: 'color',
+        object: groupBoundData, folder: groupFolder
+    }));
+    guiObjects.push(gui.Register({
+        type: 'text',
+        label: getLocalized('guiGroupDenotation'),
+        property: 'denotation',
+        object: groupBoundData, folder: groupFolder
+    }));
+    guiObjects.push(gui.Register({
+      type: 'button',
+      label: getLocalized('guiNewMeasure'),
+      folder: groupFolder
+    }));
+    guiObjects.push(...this._newMeasure({
+      gui: gui, parentFolder: groupFolder,
+      newFolderName: `${getLocalized('baseMeasure')}${uniqueSpaces}`,
+      boundData: groupBoundData.baseMeasure,
+      open: false,
+      removable: false
+    }));
+    guiObjects.push(...this._newMeasure({
+      gui: gui, parentFolder: groupFolder, 
+      newFolderName: `${getLocalized('measure')}-${0}${uniqueSpaces}`,
+      boundData: groupBoundData.measures[0], 
+      open: false,
+      removable: true
+    }));
+    this.groups.push({guiObjects: guiObjects, data: groupBoundData});
+  }
+
+  removeAllGroups(gui) {
+    this.groups.forEach(group => group.guiObjects.forEach(obj => gui.Remove(obj)));
+  }
+}
+
 function setupGui(gui, panel) {
   if (gui !== undefined) {
     let guifyContainers = document.getElementsByClassName('guify-container');
@@ -80,7 +196,7 @@ function setupGui(gui, panel) {
     width: panel.guiWidth,
     barMode: 'above', // none, overlay, above, offset
     panelMode: 'inner',
-    panelOverflowBehavior: 'overflow',
+    panelOverflowBehavior: 'scroll',
     pollRateMS: 300,
     opacity: panel.guiOpacity,
     open: true
@@ -117,6 +233,12 @@ function setupGui(gui, panel) {
     },
 
     {
+      type: 'folder',
+      label: getLocalized('measuresFolder'),
+      open: true
+    },
+
+    {
       type: 'checkbox',
       label: getLocalized('guiAskUnsaved'),
       property: 'guiAskUnsaved',
@@ -127,7 +249,6 @@ function setupGui(gui, panel) {
       type: 'file',
       label: getLocalized('openImageFiles'),
       object: this,
-      property: 'file',
       onChange: (data) => {
         if (!generalSettings.guiAskUnsaved || confirm(getLocalized('loosingUnsavedDialog'))) {
           myLoadImage(data);
@@ -159,7 +280,7 @@ function setupGui(gui, panel) {
     {
       type: 'select',
       label: getLocalized('guiWidth'),
-      options: [275, 350, 600],
+      options: [300, 375, 600],
       object: panel,
       property: 'guiWidth',
       onChange: (data) => {
@@ -265,6 +386,15 @@ function myLoadImage(uri) {
     imgView = new ImageAABBView(img.width, img.height);
     imageInfo.imageSizeStr = `${img.width} x ${img.height}`;
     resetZoom();
+    if (guiMeasureComposer !== undefined) {
+      guiMeasureComposer.removeAllGroups(gui);
+    }
+    guiMeasureComposer = new MeasureGroupGuiComposer(img.width, img.height);
+    // if (uri.includes('000006')) {
+      guiMeasureComposer.newGroup(gui, getLocalized('measuresFolder'));
+      guiMeasureComposer.newGroup(gui, getLocalized('measuresFolder'));
+    // }
+
     print('Loaded image', uri.slice(0, 100));
   });
   
@@ -277,7 +407,7 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   windowResized();
-  var gui = setupGui(gui, generalSettings);
+  gui = setupGui(gui, generalSettings);
   myLoadImage('./assets/example_trees/000006.jpg');
 }
 
@@ -305,12 +435,14 @@ function drawZoomRect(bbox) {
 
 function draw() {
   clear();
-  let zoomScreenBbox = imgView.toScreenBbox(
-    windowWidth, windowHeight,
-    zoomSettings.zoom, zoomSettings.panX, zoomSettings.panY);
-  image(img,
-    zoomScreenBbox.left, zoomScreenBbox.top,
-    zoomScreenBbox.width, zoomScreenBbox.height);
+  if (imgView !== undefined) {
+    let zoomScreenBbox = imgView.toScreenBbox(
+      windowWidth, windowHeight,
+      zoomSettings.zoom, zoomSettings.panX, zoomSettings.panY);
+    image(img,
+      zoomScreenBbox.left, zoomScreenBbox.top,
+      zoomScreenBbox.width, zoomScreenBbox.height);
+  }
 
   if (crosshairSettings.crosshairEnabled) {
     drawCrosshair({
@@ -320,7 +452,6 @@ function draw() {
       colorHexStr: crosshairSettings.crosshairColor,
     });
   }
-
 }
 
 function windowResized() {
